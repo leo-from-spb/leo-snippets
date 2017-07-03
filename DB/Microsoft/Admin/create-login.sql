94e9b878913904c3374cdd494631ac4b56443f32
Leonid Bushuev <leonid.bushuev@jetbrains.com>
2017-07-03 17:52:17 +0200

    Azure: create login and user

---
+++ DB/Microsoft/Admin/create-login.sql
@@ -0,0 +1,4 @@
+-- in the master database
+
+create login Tester with password = 'Test1234'
+go
---
+++ DB/Microsoft/Admin/create-user.sql
@@ -0,0 +1,16 @@
+-- before creating user, create a login being logged in to the master database
+
+
+-- in the necessary database
+
+create user tester from login Tester
+go
+
+exec sp_addrolemember 'db_ddladmin', 'tester'
+exec sp_addrolemember 'db_datawriter', 'tester'
+
+create schema tests authorization tester
+go
+
+alter user tester with default_schema = tests
+go
