e4ca0431a997676830d6a33da941df6cbb0b45ad
Leonid Bushuev <leonid.bushuev@jetbrains.com>
2018-04-09 13:29:06 +0200

    Oracle: Wrapped package

---
+++ DB/Oracle/Testing/package-wrapping.sql
@@ -0,0 +1,45 @@
+create package Roll
+as
+    procedure pl (s string);
+end Roll;
+/
+
+create or replace package body Roll
+as
+    procedure pl(s string)
+    is
+    begin
+        sys.dbms_output.put_line(s);
+    end;
+end Roll;
+/
+
+
+begin
+    Roll.pl('Test before wrapping');
+end;
+/
+
+declare
+    text varchar(2000);
+begin
+    text := sys.dbms_metadata.get_ddl('PACKAGE_BODY', 'ROLL', user);
+    text := trim(replace(text, 'EDITIONABLE', ''));
+    --sys.dbms_output.put_line(text);
+    sys.dbms_ddl.create_wrapped(text);
+end;
+/
+
+alter package Roll compile body
+/
+
+begin
+    Roll.pl('Test after wrapping');
+end;
+/
+
+select *
+from sys.user_source
+where name = 'ROLL'
+  and type = 'PACKAGE BODY'
+/
