871a61ebb58b1c37813912d2cf333dfc09c57529
Leonid Bushuev <leonid.bushuev@jetbrains.com>
2019-06-26 21:17:59 +0300

    Oracle: simple package for debugging

--- DB/Oracle/Testing/Debug/rabbit.sql
+++ DB/Oracle/Testing/Debug/rabbit.sql
@@ -1,3 +1,39 @@
+create or replace package Simplex
+as
+    --
+    counter binary_integer := 0;
+    --
+    procedure inc_counter;
+    pragma restrict_references (inc_counter, RNDS, WNDS);
+    --
+    procedure inc_counter_N_times (times binary_integer);
+    pragma restrict_references (inc_counter_N_times, RNDS, WNDS);
+    --
+end Simplex;
+/
+
+create or replace package body Simplex
+as
+    --
+    procedure inc_counter is
+    begin
+        counter := counter + 1;
+    end;
+    --
+    procedure inc_counter_N_times (times binary_integer) is
+    begin
+        for i in 1 .. times
+            loop
+                inc_counter();
+            end loop;
+    end;
+    --
+end Simplex;
+/
+
+
+
+
 create or replace procedure Pro_B as
 begin
     dbms_output.put_line('Procedure Pro_B');
@@ -30,6 +66,8 @@ begin
 end;
 /
 
+alter package Simplex compile debug;
+alter package Simplex compile debug body;
 alter procedure Pro_B compile debug;
 alter procedure Pro_C compile debug;
 alter procedure Pro compile debug;
