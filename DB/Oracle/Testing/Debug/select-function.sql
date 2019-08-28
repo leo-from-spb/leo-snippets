f84a9c3004853c5c0c7fa2ecee22e83f0bc8a43f
Leonid Bushuev <leonid.bushuev@jetbrains.com>
2019-08-28 19:00:36 +0300

    Oracle: debug-related files

--- DB/Oracle/Testing/Debug/select-function.sql
+++ DB/Oracle/Testing/Debug/select-function.sql
@@ -7,7 +7,23 @@ end;
 alter function rnd compile debug
 /
 
+
+declare
+    x natural;
+begin
+    select count(*)
+    into x
+    from dual
+    where rnd < 100 or rnd = 0;
+end;
+/
+
+
 select rnd
+from dual
+/
+
+select rnd, rnd
 from dual
 /
 
