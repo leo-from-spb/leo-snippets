fd6dcb119195fe50d4f09dc19d2e453c886c0036
Leonid Bushuev <leonidos@gmail.com>
2020-11-16 13:27:20 +0100

    Oracle: better example on materialized view on pre-built table

--- DB/Oracle/Testing/mat-view-prebuilt.sql
+++ DB/Oracle/Testing/mat-view-prebuilt.sql
@@ -1,25 +1,52 @@
-create table mat_owners
+create table Strange_Original_Table
 (
-    name varchar(30) primary key
+    Id number(6) not null
+        constraint Strange_Original_Table_pk primary key,
+    Status number(1) default 0 not null
 )
-    organization index
 /
 
-create materialized view mat_owners
+
+create table Strange_Stat
+(
+    Status number(1) not null
+        constraint Strange_Stat_pk primary key,
+    Cnt number(9) not null
+)
+/
+
+
+create materialized view Strange_Stat
     on prebuilt table
-    refresh complete
-as
-select distinct owner as name
-    from sys.all_mviews
+    refresh complete on demand
+    as
+select Status, cast(count(*) as number(9)) as Cnt
+from Strange_Original_Table
+group by Status
 /
 
-alter materialized view mat_owners refresh complete
+
+begin
+    insert into Strange_Original_Table values (1001, 1);
+    insert into Strange_Original_Table values (1002, 2);
+    insert into Strange_Original_Table values (1003, 2);
+    insert into Strange_Original_Table values (1004, 3);
+    insert into Strange_Original_Table values (1005, 3);
+    insert into Strange_Original_Table values (1006, 3);
+    insert into Strange_Original_Table values (1007, 3);
+end;
 /
 
+commit
+/
 
-alter materialized view mat_owners refresh complete
+begin
+    dbms_mview.Refresh('STRANGE_STAT', 'C');
+end;
 /
 
 select *
-from mat_owners
+from Strange_Stat
 /
+
+
